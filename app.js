172e45387bcafabec0a74bbfcb896d9db0582e72
florianajir <florianajir@gmail.com>
2017-09-02 22:40:51 +0400

    refactoring and log errors

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// var bootstrap = require('bootstrap')
-var mongoose = require('mongoose');
+var expressValidator = require('express-validator');
 var session = require('express-session');
+var mongoose = require('mongoose');
 var MongoStore = require('connect-mongo')(session);
-mongoose.connect('mongodb://localhost/dronemap');
+
+var app = express();
+
+// database connect
+var mongoDB = 'mongodb://localhost/dronemap';
+mongoose.connect(mongoDB);
 var db = mongoose.connection;
 
 //handle mongo error
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
   // we're connected!
 });
-
-var index = require('./routes/index');
-var auth = require('./routes/auth');
-
-var app = express();
 
 //use sessions for tracking logins
 app.use(session({
@@ -28,37 +28,43 @@ app.use(session({
   resave: true,
   saveUninitialized: false,
   store: new MongoStore({
-    mongooseConnection: db
-  })
+    mongooseConnection: db,
+  }),
 }));
 app.locals.inspect = require('util').inspect;
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/js', express.static(__dirname + '/node_modules/popper.js/dist'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap'));
-app.use('/font-awesome', express.static(__dirname + '/node_modules/font-awesome'));
-app.use('/open-iconic', express.static(__dirname + '/node_modules/open-iconic'));
+app.use('/font-awesome',
+    express.static(__dirname + '/node_modules/font-awesome'));
+app.use('/open-iconic',
+    express.static(__dirname + '/node_modules/open-iconic'));
 
 app.use(function(req, res, next) {
-  console.log(req.session);
+  res.locals.userId = req.session.userId;
   res.locals.user = req.session.user;
   next();
 });
 
+// routes
+var index = require('./routes/index');
+var users = require('./routes/users');
 app.use('/', index);
-app.use('/auth', auth);
+app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -67,12 +73,17 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
+function logErrors (err, req, res, next) {
+  console.error(err.stack);
+  next(err);
+}
+app.use(logErrors);// error handler
+
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  logErrors(err,req,res,next);
   // render the error page
   res.status(err.status || 500);
   res.render('error');
