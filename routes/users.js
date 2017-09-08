e89ff0b0422defc9671c5c06f225afd125092498
florianajir <florianajir@gmail.com>
2017-09-08 16:05:05 +0400

    fix avatar upload with multer middleware

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const multer = require('multer');
+const upload = multer({dest: 'public/uploads/'});
 const router = express.Router();
 const auth_controller = require('../controllers/authController');
 const profile_controller = require('../controllers/profileController');
 
 router.get('/profile/:username', profile_controller.user_view_get);
 router.get('/profile/:username/edit', profile_controller.user_edit_get);
-router.post('/profile/:username/edit', profile_controller.user_edit_post);
+router.post('/profile/:username/edit', upload.single('avatar'), profile_controller.user_edit_post);
 router.get('/profile', profile_controller.user_profile_get);
 router.get('/login', auth_controller.user_login_get);
 router.post('/login', auth_controller.user_login_post);
