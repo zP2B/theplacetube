7a9cfde2b5fec0ad8853dd942cfcb645fe8c7582
florianajir <florianajir@gmail.com>
2017-10-21 21:57:55 +0700

    add forever-monitor to project to keep alive node process longer in prod

---
+++ forever.js
@@ -0,0 +1,24 @@
+const forever = require('forever-monitor');
+
+const child = new (forever.Monitor)('bin/www', {
+  max: 10,
+  silent: true,
+  args: [],
+  logFile: 'logs/forever.out',
+  outFile: 'logs/log.out',
+  errFile: 'logs/err.out'
+});
+
+child.on('restart', function() {
+  console.error('Forever restarting script for ' + child.times + ' time');
+});
+
+child.on('exit:code', function(code) {
+  console.error('Forever detected script exited with code ' + code);
+});
+
+child.on('exit', function() {
+  console.log('server has exited after 3 restarts');
+});
+
+child.start();
