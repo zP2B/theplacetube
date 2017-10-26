1f2f6906e53a23c99fcf2b4d4a9d6fb4fc4efbc7
florianajir <florianajir@gmail.com>
2017-10-27 00:35:04 +0700

    refactoring geolib service

--- services/geolib.js
+++
@@ -1,9 +0,0 @@
-const geolib = require('geolib');
-
-exports.getDistanceBetween = function(coords1, coords2, accuracy = 10) {
-  return geolib.getDistanceSimple(
-      {latitude: coords1.lat, longitude: coords1.lng},
-      {latitude: coords2.lat, longitude: coords2.lng},
-      accuracy
-  );
-};
